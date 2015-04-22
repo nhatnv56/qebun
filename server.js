79eb97452f79c6e064ad826c945c510ac64cffdf
Nhat <nhatnv_56@vnu.edu.vn>
2015-04-22 04:20:56 -0400

    :_)

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ var server = http.createServer(function (req, res) {
   res.end('Hello World\n');
 });
 var port = Number(process.env.PORT || 3000);
-server.listen(1337);
+server.listen(port);
 console.log('Server running at http://127.0.0.1:1337/');
