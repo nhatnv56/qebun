377f8395f42811399a3224cfa4abb7152a4d5545
Nhat <nhatnv_56@vnu.edu.vn>
2015-04-22 05:51:28 -0400

    :_)

--- server.js
+++ server.js
@@ -14,8 +14,9 @@ app.use(session({secret: 'ssshhhhh'}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname+'/public'));
-var uristring = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://xedap100:perona100@ds041851.mongolab.com:41851/demo';
+
 //mongoose.connect('mongodb://localhost/test');
+var uristring = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://xedap:perona100@ds041851.mongolab.com:41851/demo';
 mongoose.connect(uristring,function(err,res) {
     if (err) {
         console.log ('ERROR connecting to: ' + uristring + '. ' + err);
