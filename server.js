058f12b46e94da95d6360e256c448f33584e03cf
Nhat <nhatnv_56@vnu.edu.vn>
2015-04-22 04:06:41 -0400

    my first commit

---
+++ server.js
@@ -0,0 +1,8 @@
+var http = require('http');
+var server = http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+});
+var port = Number(process.env.PORT || 3000);
+server.listen(1337);
+console.log('Server running at http://127.0.0.1:1337/');
