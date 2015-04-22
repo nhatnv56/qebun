ba914b822bbc2d01ea0e2b8874398565ac531d72
Nhat <nhatnv_56@vnu.edu.vn>
2015-04-22 05:00:00 -0400

    nhu cut

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 app.set('port', (process.env.PORT || 5000));
+var uristring = process.env.MONGOLAB_URI 
+    || process.env.MONGOHQ_URL
+    || 'mongodb://localhost/HelloMongoose';
+
 app.set('views', __dirname + '/views');
 app.engine('.html',require('ejs').__express);
 app.set('view engine','html');
@@ -15,7 +19,14 @@ app.use(session({secret: 'ssshhhhh'}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname+'/public'));
-mongoose.connect('mongodb://localhost/test');
+//mongoose.connect('mongodb://localhost/test');
+mongoose.connect(uristring, function (err, res) {
+  if (err) {
+  console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+  console.log ('Succeeded connected to: ' + uristring);
+  }
+});
 
 var UserSchema = new Schema({
     email:{type: String, required: true},
